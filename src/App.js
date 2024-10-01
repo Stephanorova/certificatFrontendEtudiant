661265ab60b34000fc180b00543e90597e1e856d
unknown <garoust60@gmail.com>
2024-10-01 10:52:42 +0300

    formulaire de verification d'un certificat d'etudiant

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Card, CardHeader, CardContent, TextField, Button, Typography } from '@mui/material';
+import Navebar from './Navebar/Navebar';
 
 function App() {
+
+  const [certificateNumber, setCertificateNumber] = useState('');
+  const [verificationResult, setVerificationResult] = useState(null);
+  const [error, setError] = useState('');
+
+  const handleVerification = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`http://localhost:3000/api/verify/${certificateNumber}`);
+      setVerificationResult(response.data);
+      setError('');
+    } catch (err) {
+      setVerificationResult(null);
+      setError('Erreur lors de la vérification du certificat.');
+    }
+  };
+
+  const handleDownload = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/pdf/${certificateNumber}`, {
+        responseType: 'blob'
+      });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `${certificateNumber}.pdf`);
+      document.body.appendChild(link);
+      link.click();
+    } catch (err) {
+      setError('Erreur lors du téléchargement du PDF.');
+    }
+  };
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <Navebar/>
     </div>
+   <div style={{ padding: '20px',marginTop:"3rem", maxWidth: '600px', margin: '0 auto' }}>
+      <Typography variant="h5" gutterBottom>
+        Vérification de Certificat Étudiant
+      </Typography>
+      
+      <form onSubmit={handleVerification} style={{ marginBottom: '20px' }}>
+        <TextField
+          fullWidth
+          label="Numéro de Certificat"
+          variant="outlined"
+          value={certificateNumber}
+          onChange={(e) => setCertificateNumber(e.target.value)}
+          style={{ marginBottom: '10px' }}
+          required
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Vérifier
+        </Button>
+      </form>
+
+      {error && (
+        <Typography color="error" gutterBottom>
+          {error}
+        </Typography>
+      )}
+
+      {verificationResult  && (
+        <Card>
+          <CardHeader title="Résultat de votre vérification" />
+          <CardContent>
+            <Typography variant="body1" gutterBottom>
+              <strong>Numéro de certificat:</strong>{verificationResult.certificateNumber}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              <strong>Nom de l'étudiant:</strong> {verificationResult.studentName}
+            </Typography>
+            <Button variant="contained" color="success" type="submit"  onClick={handleDownload}>
+                  Télécharger votre certificat
+               </Button>
+          </CardContent>
+        </Card>
+      )}
+    </div> 
+    </>
   );
 }
 
